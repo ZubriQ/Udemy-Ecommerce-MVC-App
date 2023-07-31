20880ed8bef48f33b0fd0cea046b378e6feff101
ZubriQ <zubrich157@gmail.com>
2023-07-31 19:38:34 +0700

    Fix actor movie properties

--- LastFilm-Web-App/Models/ActorMovie.cs
+++ LastFilm-Web-App/Models/ActorMovie.cs
@@ -3,7 +3,7 @@
 public class ActorMovie
 {
     public int ActorId { get; set; }
-    public Actor Actor { get; set; }
+    public Actor Actor { get; set; } = null!;
     public int MovieId { get; set; }
-    public Movie Movie { get; set; }
+    public Movie Movie { get; set; } = null!;
 }
